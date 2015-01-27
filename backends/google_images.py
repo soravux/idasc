74985a9827f8d7772cd7d8ccd51a221c113ed623
Yannick Hold <yannickhold@gmail.com>
2015-01-27 11:01:05 -0500

    Updated Google backend to Custom Search

--- backends/google_images.py
+++ backends/google_images.py
@@ -5,34 +5,45 @@ import requests
 from progressbar import ProgressBar
 
 from .common.download import downloadImage
+from ._config_parser import config
 
 
-# TODO: This API is deprecated and limited to 8 pages.
-# Use customsearch API instead
-BASE_URL = ("https://ajax.googleapis.com/ajax/services/search/images"
-            "?v=1.0&q={query}&start={start}")
+API_KEY = config.get('google', 'API_KEY')
+NB_IMAGES = int(config.get('google', 'NB_IMAGES'))
+cx = config.get('google', 'cx')
+
+ROOT_URL = "https://www.googleapis.com/customsearch/v1?"
+BASE_URL = (ROOT_URL + "cx={cx}&q={query}&searchType=image&key={API_KEY}&start={start}")
 
 
 def performRequest(query, BASE_PATH, start):
     """
     Perform a JSON request to google images.
     API reference:
-    https://developers.google.com/image-search/v1/jsondevguide
+    http://developers.google.com/apis-explorer/
     """
-    r = requests.get(BASE_URL.format(query=query, start=start))
+    r = requests.get(BASE_URL.format(query=query, cx=cx, API_KEY=API_KEY, start=start))
     response = json.loads(r.text)
-    for image_info in response['responseData']['results']:
-        url = image_info['unescapedUrl']
+    for image_info in response['items']:
+        url = image_info['link']
         downloadImage(url, BASE_PATH)
+        img_done += 1
+        progress.update(img_done)
+
+    return response['queries']['nextPage'][0]['count']
 
 def go(query, path):
     """
     Download full size images from Google image search.
     """
-    # Maximum 8 pages as written in the JSON dev guide of Google Image
-    progress = ProgressBar()
-    for i in progress(range(8)):
-        performRequest(query, path, i)
+    global progress, img_done
+    progress = ProgressBar(maxval=NB_IMAGES).start()
+    img_done = 0
+    while img_done < NB_IMAGES:
+        img_done += performRequest(query, path, img_done + 1)
+        progress.update(img_done)
+    progress.finish()
+
 
 if __name__ == '__main__':
     # Example use
